2c519b9dc3c245455b4c5eca994b10111c9e774b
hayounglee <home0@naver.com>
2023-08-19 10:36:06 +0900

    feature: 마커들 클로저로 컨트롤, 클릭시마다 마커 추가하기

--- src/App.tsx
+++ src/App.tsx
@@ -19,6 +19,30 @@ const positions = [
 		latlng: new kakao.maps.LatLng(33.451393, 126.570738),
 	},
 ];
+const fetchOption = {
+	method: 'GET',
+	headers: {
+		Authorization: 'KakaoAK 9a8bb55a94f70533c68ddc084e58be33',
+	},
+};
+
+// 인포윈도우를 표시하는 클로저를 만드는 함수입니다
+function makeOverListener(
+	map: kakao.maps.Map,
+	marker: kakao.maps.Marker,
+	infowindow: kakao.maps.InfoWindow
+) {
+	return function () {
+		infowindow.open(map, marker);
+	};
+}
+
+// 인포윈도우를 닫는 클로저를 만드는 함수입니다
+function makeOutListener(infowindow: kakao.maps.InfoWindow) {
+	return function () {
+		infowindow.close();
+	};
+}
 
 function App() {
 	const container = useRef<HTMLDivElement | null>(null);
@@ -28,12 +52,6 @@ function App() {
 	const [reactMap, setReactMap] = useState<kakao.maps.Map | null>(null);
 	const [markerArray, setMarkerArray] = useState<kakao.maps.Marker[]>([]);
 
-	/**
-	 * 지도 클릭시 마커를 이동하고 위도, 경도를 표시한다.
-	 * @param mouseEvent : 이벤트
-	 * @param marker : 마커객체
-	 */
-
 	//시작 : kakao map 글로벌객체를 state에 담는다.(나중에 쓰기편하게)
 	useEffect(() => {
 		if (!container.current) return;
@@ -41,10 +59,11 @@ function App() {
 		setInitialKaKao(kakao);
 	}, [container]);
 
-	//카카오맵을 그리고, 마커도 그리고, 지도클릭시에 마커이동
+	//비즈니스 로직 분리(마커 추가, 복수의 마커 등등)
 	useEffect(() => {
 		if (!initialKakao) return;
 		if (!container.current) return;
+
 		const options = {
 			center: new initialKakao.LatLng(33.450701, 126.570667),
 			level: 3,
@@ -54,6 +73,46 @@ function App() {
 
 		setReactMap(map);
 
+		//클릭이벤트로 위도, 경도 불러오고, 추가된 마커의 주소 불러오기
+		initialKakao.event.addListener(map, 'click', async (mouseEvent: any) => {
+			if (!initialKakao) return;
+			const marker = new initialKakao.Marker({
+				position: mouseEvent.latLng,
+			});
+			const latlng = mouseEvent.latLng;
+			// const newMessage = `클릭한 위치의 위도는 ${latlng.getLat()} 이고, 경도는 ${latlng.getLng()} 입니다`;
+
+			const getLocation = await fetch(
+				`${`https://dapi.kakao.com/v2/local/geo/coord2address.json?x=${latlng.getLng()}&y=${latlng.getLat()}`}`,
+				fetchOption
+			);
+			const jsonData = await getLocation.json();
+			const result = await jsonData.documents[0].address.address_name;
+			marker.setTitle(result);
+			marker.setMap(map);
+
+			setMarkerArray([...markerArray, marker]);
+
+			// 마커에 표시할 인포윈도우를 생성합니다
+			const infowindow = new kakao.maps.InfoWindow({
+				content: result, // 인포윈도우에 표시할 내용
+			});
+
+			kakao.maps.event.addListener(
+				marker,
+				'mouseover',
+				makeOverListener(map, marker, infowindow)
+			);
+			kakao.maps.event.addListener(
+				marker,
+				'mouseout',
+				makeOutListener(infowindow)
+			);
+			kakao.maps.event.addListener(marker, 'click', () => {
+				marker.setMap(null);
+				infowindow.close();
+			});
+		});
 		//복수의 마커들
 		for (let i = 0; i < positions.length; i++) {
 			// 마커를 생성합니다
@@ -63,26 +122,23 @@ function App() {
 				title: positions[i].title, // 마커의 타이틀, 마커에 마우스를 올리면 타이틀이 표시됩니다
 			});
 			markers.setMap(map);
-		}
 
-		//클릭이벤트
-		initialKakao.event.addListener(map, 'click', (mouseEvent: any) => {
-			if (!initialKakao) return;
-
-			// const latlng = mouseEvent.latLng;
-			const marker = new initialKakao.Marker({
-				position: mouseEvent.latLng,
+			// 마커에 표시할 인포윈도우를 생성합니다
+			const infowindow = new kakao.maps.InfoWindow({
+				content: positions[i].title ?? positions[i].title, // 인포윈도우에 표시할 내용
 			});
 
-			// 마커가 지도 위에 표시되도록 설정합니다
-			marker.setMap(map);
-
-			// // 생성된 마커를 배열에 추가합니다
-			setMarkerArray([...markerArray, marker]);
-		});
-
-		// return () =>
-		// 	initialKakao.event.removeListener(map, 'click', handleMapClick);
+			kakao.maps.event.addListener(
+				markers,
+				'mouseover',
+				makeOverListener(map, markers, infowindow)
+			);
+			kakao.maps.event.addListener(
+				markers,
+				'mouseout',
+				makeOutListener(infowindow)
+			);
+		}
 	}, [initialKakao, container]);
 
 	//컨트롤바
